ba9b2c37fd7ec13df95861ffa045f17c02a0a02e
Quant Analytist <quant.analytics.torch@gmail.com>
2020-12-04 20:28:41 +0000

    First commit

---
+++ tests/tensor_test.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+
+EPSILON = 0.0001
+
+def test_zero():
+
+    assert abs(0.0) < EPSILON
+
+
+def test_another_zero():
+    
+    assert abs(0.0) < EPSILON
+
+if __name__ == '__main__':
+    
+    test_zero()
